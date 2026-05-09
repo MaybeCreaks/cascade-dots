8c6f364012572eb49f068a5dc390cdda09809cca
Creaks. <contact.cxsmicpvps@gmail.com>
2026-05-09 23:54:12 +0300

    Add files via upload

---
+++ dots/graphite/graphite.sh
@@ -0,0 +1,1 @@
+cd ~/ && git clone https://github.com/vinceliuice/Graphite-gtk-theme.git && cd Graphite-gtk-theme && sudo ./install.sh -c dark -l --tweaks black
---
+++ dots/tela/tela.sh
@@ -0,0 +1,1 @@
+cd ~/ && git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git && cd Tela-circle-icon-theme && sudo ./install.sh black nord
